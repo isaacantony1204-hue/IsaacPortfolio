dcf11353aa902e308cb04f15ade3a2948a8cee16
isaacantony1204-hue <isaacantony1204@gmail.com>
2026-07-04 23:35:08 +0530

    .

--- script.js
+++ script.js
@@ -1,30 +1,17 @@
-document.getElementById("contactForm").addEventListener("submit", async function (e) {
+document.getElementById("contactForm").addEventListener("submit", async function(e){
   e.preventDefault();
 
-  // collect form data
   const data = {
     name: document.getElementById("name").value,
     email: document.getElementById("email").value,
     message: document.getElementById("message").value
   };
 
-  try {
-    // send to Google Apps Script
-    const response = await fetch("https://script.google.com/macros/s/AKfycbyrEMH9GTj76TYZsSdzOwtjjByYEhTRtCqZvNKpY-83SXgIICIqUUQirgnRJysAsZCI-A/exec", {
-      method: "POST",
-      body: JSON.stringify(data)
-    });
+  await fetch("https://script.google.com/macros/s/AKfycbyrEMH9GTj76TYZsSdzOwtjjByYEhTRtCqZvNKpY-83SXgIICIqUUQirgnRJysAsZCI-A/exec", {
+    method: "POST",
+    body: JSON.stringify(data)
+  });
 
-    // success feedback
-    if (response.ok) {
-      alert("Message sent 🚀");
-      this.reset();
-    } else {
-      alert("Something went wrong 💀");
-    }
-
-  } catch (error) {
-    console.log(error);
-    alert("Network error 💀 check console");
-  }
+  alert("Message sent 🚀");
+  this.reset();
 });
